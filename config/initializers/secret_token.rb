d422750f8e422693f749815f67fbf194472f5a59
Brian Gilbreath <bgilbreath87@gmail.com>
2010-11-15 18:55:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubynuby::Application.config.secret_token = 'd5ec2c932d54b107bacc3d780706cba7d3eb035189d847a57b057bdae09d3215eb3c8387c90144fdf1499d63c7dff6eedef1c350acb81feaee8a6ffd7f6ee50a'
